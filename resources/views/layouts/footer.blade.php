fab257d9f626132824c6fbbba56891c3f722ed7a
Besta Alfidi <43779424+alfidibesta@users.noreply.github.com>
2021-11-18 07:38:43 +0700

    update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container-fluid clearfix">
     <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©  <a href="http://bri.co.id/" target="_blank">BRILink Kanwil Malang</a> 2021</span>
-    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Free <a href="https://www.bootstrapdash.com/laravel-templates/" target="_blank">Laravel template</a> by BootstrapDash.com | Besta Alfidi
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="" target="_blank">ERM BRI Kanwil v2</a>
     </span>
   </div>
 </footer>
